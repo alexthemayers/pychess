c37258874865b8183302d19c139b60c476e8e3c1
Alexander Mayers <62880419+alexthemayers@users.noreply.github.com>
2022-07-04 15:55:15 +0200

    added constants file

---
+++ main.py
@@ -0,0 +1,14 @@
+# from . import Board
+# from . import Pieces
+# from . import Logic
+# from . import Input
+from Constants import *
+
+
+
+def main():
+    print(BOARD_WIDTH)
+    pass
+
+if __name__ == "__main__":
+    main()
