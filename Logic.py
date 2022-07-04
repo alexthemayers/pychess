0df4cb46360deef82a1a7cc55ab5775938efdd86
Callum McDonald <callummcdonald@Heypapalegend-CTMini.local>
2022-07-04 17:04:22 +0200

    updated logic.py :)

--- Logic.py
+++ Logic.py
@@ -1,16 +1,21 @@
-from wsgiref.simple_server import WSGIRequestHandler
-from Piece import *
-from Constants import *
 
 
-def isLegal(p: Piece) -> bool:
-    if p.name == ROOK:
-        pass
-    elif p.name == QUEEN: 
-        pass
-    
-    
-    
-if __name__ == "__main__":
-    print("run main.py")
-    exit(1)
+class Outcome():
+	def __init__(self):
+
+	def is_legal():
+# if the target square is available move to piece
+# moving into check? that's not allowed
+# then if target square is occupied, you are attacking!
+
+	def is_check():
+# king is attacked and can move or block attack
+# 
+	def is_checkmate():
+# if King is in check, has 0 available moves, and attack can't be blocked
+
+	def is_stalemate():
+# current player has 0 available moves
+
+	def is_draw():
+# player accepted player "draw" request
