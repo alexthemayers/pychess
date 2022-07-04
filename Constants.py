9f5b41d439301d5da86e7bdd368b1f0f8f6d9a60
Alexander Mayers <62880419+alexthemayers@users.noreply.github.com>
2022-07-04 16:27:56 +0200

    progress

--- Constants.py
+++ Constants.py
@@ -1,2 +1,24 @@
 BOARD_WIDTH = 8
 BOARD_HEIGHT = 8
+
+class Piece():
+    
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def isPiece(self):
+        return f"i am a {self.name} \n"
+
+class Rook(Piece):
+    
+    def __init__(self): 
+        pass
+    
+    def isPiece(self):
+        return "I am a rook"
+    
+r = Piece("rook")
+
+print(r.isPiece())
+
